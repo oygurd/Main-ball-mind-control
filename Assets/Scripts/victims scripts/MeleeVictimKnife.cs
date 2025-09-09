5553f663567fef61a8d03d589fe0376d8b949374
Nir Mula <nirmula3d@gmail.com>
2025-09-10 01:19:41 +0300

    bit added to the scriptable code

--- Assets/Scripts/victims scripts/MeleeVictimKnife.cs
+++ Assets/Scripts/victims scripts/MeleeVictimKnife.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "MeleeVictimKnife", menuName = "Scriptable Objects/MeleeVictimKnife")]
+[CreateAssetMenu(fileName = "MeleeVictimKnife", menuName = "VictimsClasses/MeleeVictimKnife", order = 1)]
 public class MeleeVictimKnife : ScriptableObject
 {
-    
+    public string name;
+    public int health;
+    public int strength;
+    public float speed;
 }

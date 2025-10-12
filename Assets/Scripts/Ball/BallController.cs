05ee16d5bed307fa44ce74e9b020c12da9b34c90
Nir Mula <nirmula3d@gmail.com>
2025-10-13 00:14:03 +0300

    copy paste ball

--- Assets/Scripts/Ball/BallController.cs
+++ Assets/Scripts/Ball/BallController.cs
@@ -3,15 +3,20 @@ using UnityEngine;
 
 public class BallController : SerializedMonoBehaviour
 {
+    [SerializeField] private bool isHeld;
+
+    [SerializeField] Transform ballTransform;
+    [SerializeField] Rigidbody2D ballRb;
+
+    [SerializeField] CircleCollider2D ballCollider;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }

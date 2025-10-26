7e861b609fab91458bed94bf752ee7eff2dfa9d3
Nir Mula <nirmula3d@gmail.com>
2025-10-26 10:18:28 +0200

    attacksmanager

--- Assets/Scripts/Weapons/MeleeClassVictim.cs
+++ Assets/Scripts/Weapons/MeleeClassVictim.cs
@@ -1,11 +1,22 @@
+using System;
 using UnityEngine;
 using Sirenix.OdinInspector;
+using UnityEngine.InputSystem;
 public class MeleeClassVictim : SerializedMonoBehaviour
 {
     public MeleeWeaponParameters MeleeWeaponParameters;
     
     public Melee_AnimationsHandler melee_animationsHandler;
-    
+
+    public InputAction BasicAttackInput;
+    public InputAction ParryInput;
+    public InputAction ThrowableInput;
+
+    private void Awake()
+    {
+        
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {

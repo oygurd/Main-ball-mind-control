066d0a11b2e7c41a8eb0c85f66d0629a5fcefb01
Nir Mula <nirmula3d@gmail.com>
2025-11-05 00:05:06 +0200

    debug raycast and raycasthit

--- Assets/Scripts/Weapons/MeleeClassVictim.cs
+++ Assets/Scripts/Weapons/MeleeClassVictim.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using Nomnom.RaycastVisualization;
 using UnityEngine;
 using Sirenix.OdinInspector;
+using Unity.VisualScripting;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Melee_AnimationsHandler))]
@@ -12,7 +14,23 @@ public class MeleeClassVictim : SerializedMonoBehaviour
     public Melee_AnimationsHandler melee_animationsHandler;
 
     InputAction melee_inputAction;
-    float raycastDuration;
+
+    public Transform VictimTransformReference;
+
+   public RaycastHit2D weaponRaycastHit;
+   public LayerMask WeaponLayerMask;
+    //public ContactFilter2D contactFilter;
+   // List<ContactFilter2D> ContactList = new List<RaycastHit2D>();
+
+    private void Update()
+    {
+        if (melee_inputAction.inProgress)
+        {
+           weaponRaycastHit = Physics2D.Raycast(transform.position,
+                transform.right * VictimTransformReference.localScale.x,
+                MeleeWeaponParameters.attackRange, WeaponLayerMask);
+        }
+    }
 
     private void Awake()
     {
@@ -20,13 +38,26 @@ public class MeleeClassVictim : SerializedMonoBehaviour
         melee_inputAction = InputSystem.actions.FindAction("Attack");
     }
 
-    public void OnAttack()
+    public void OnParry()
     {
-        if (melee_inputAction.IsInProgress())
-        {
-            VisualPhysics2D.BoxCast(transform.position, new Vector2(MeleeWeaponParameters.attackRange,2),90,transform.forward);
-        }
     }
 
+    private void OnDrawGizmos()
+    {
+        //basic attack gizmos
+        if (melee_inputAction.inProgress)
+        {
+            if (!weaponRaycastHit)
+            {
+                Gizmos.color = Color.red;
+            }
+            else if (weaponRaycastHit)
+            {
+                Gizmos.color = Color.green;
+            }
+
+            Gizmos.DrawRay(transform.position, transform.right * VictimTransformReference.localScale.x * MeleeWeaponParameters.attackRange);
+        }
+    }
     
 }

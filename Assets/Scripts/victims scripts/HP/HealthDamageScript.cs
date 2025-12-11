7779c4d2bdde9743ea04085f17b0fe84144a007b
Nir Mula <nirmula3d@gmail.com>
2025-12-11 16:41:16 +0200

    fixed basic usage of hp bar

--- Assets/Scripts/victims scripts/HP/HealthDamageScript.cs
+++ Assets/Scripts/victims scripts/HP/HealthDamageScript.cs
@@ -1,22 +1,28 @@
 using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.InputSystem;
+
 
 public class HealthDamageScript : SerializedMonoBehaviour
 {
     public Slider healthSlider;
+
+    private InputAction test;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        test = InputSystem.actions.FindAction("Test");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (test.triggered)
         {
-            healthSlider.value -= 10;
+            healthSlider.value -= 0.1f;
         }
     }
 }
